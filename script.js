f759e674042853475a6b5328e6e7cac6209590e1
ChrisLin0923 <chrislinflhs@gmail.com>
2021-11-21 14:38:24 -0500

    update

---
+++ script.js
@@ -0,0 +1,9 @@
+function openNav() {
+  document.getElementById("mySidebar").style.width = "170px";
+  document.getElementById("main").style.marginLeft = "180px";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
