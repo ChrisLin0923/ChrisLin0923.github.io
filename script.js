52eb807f78df4f1dd5a63b86300a856775322bd9
ChrisLin0923 <chrislinflhs@gmail.com>
2021-11-27 12:28:55 -0500

    update

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ function openNav() {
   document.getElementById("main").style.marginLeft = "180px";
 }
 
+
 function closeNav() {
   document.getElementById("mySidebar").style.width = "0";
   document.getElementById("main").style.marginLeft= "0";
